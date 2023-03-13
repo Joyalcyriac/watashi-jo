e560aa0f4fafec8d4871ab452bb08825834489c4
unknown <jibinpjohny000@example.com>
2023-03-13 02:41:03 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,124 @@
+// var x=0;
+// var name='joyal cyriac';
+// var y=20;
+// {
+//     var y=10
+//     console.log(y);
+// }
+// console.log(y);
+// const pi=3.14;
+// console.log(pi);
+
+// var sum=pi+10;
+// console.log(sum);
+
+// var div=y/2;
+// console.log(div);
+
+// var multi=y*pi;
+// console.log(multi);
+
+// var sub=y-pi;
+// console.log(sub);
+
+// var x=10;
+// var y=5;
+// var sum=x+y;
+// console.log(sum);
+// document.write("sum="+sum);
+
+// var f='JOYAL';
+// var s='CYRIAC';
+// document.write(`fullname is ${f} ${s}`);
+
+// var student={
+//     name:"JOYAL",
+//     age:19,
+//     place:'koratty'
+// }
+// console.log(student);
+// console.log(student.name);
+
+// var cars={
+//     model:"bugatti",
+//     color:'gold',
+//     price:500
+// }
+
+// console.log(cars.model);
+
+// var fruits=['apple','bannana','grape',10];
+// console.log(fruits);
+// console.log(fruits[0]);
+// console.log(fruits[3]);
+// console.log(fruits.len);
+// fruits[2]='watermelon';
+// fruits.push('orange');
+// fruits.unshift('kiwi');
+// console.log(fruits);
+// fruits.pop(1);
+// console.log(fruits);
+// fruits.shift()
+
+// var student=[{
+//     name:"JOYAL",
+//     age:19,
+//     place:'koratty'
+// },
+// {
+//     name:"fahad",
+//     age:19,
+//     place:'irijalakuda'
+// },
+// {
+//     name:"jerin",
+//     age:19,
+//     place:'kanakamala'
+// }]
+
+// console.log(student[1].name);
+
+// function add(a,b)
+// {
+//     var c=a+b;
+    
+//     console.log(c);
+// }
+
+// add(1,68);
+// const multi=(a,b)=>{
+
+//     var c=a*b;
+//     console.log(c);
+// }
+
+// multi(2,6);
+// var x=20;
+// if(x>30){
+//     console.log('good');
+
+// }
+// else{
+//     console.log('bad');
+// }
+
+// for(var i=1;i<10;i++)
+// {
+//     console.log(i);
+// }
+var fruits=['apple','bannana','grape'];
+// for(var i=0;i<fruits.length;i++)
+// {
+//     console.log(fruits[i]);
+// }
+// let p=0;
+// while(p<fruits.length){
+//     console.log(fruits[p]);
+//     p++
+// }
+var p=0;
+do{
+    console.log(fruits[p]);
+    p++
+}
+while(p<fruits.length)
